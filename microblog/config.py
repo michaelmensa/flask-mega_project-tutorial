e6b1d45563ad0d46b5bf47f347ac9bbf1907a56d
michaelmensa <michaelmensa@live.com>
2023-11-17 21:02:38 +0000

    changes email

--- microblog/config.py
+++ microblog/config.py
@@ -19,4 +19,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['michaelmensa@live.com']
+    ADMINS = ['avatarmensa@gmail.com']
