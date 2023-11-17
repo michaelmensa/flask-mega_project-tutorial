682c28094c858f2da890ed3a1a57f61401e50ef2
Michael Mensah <michaelmensa@live.com>
2023-11-17 09:27:33 +0000

    spelling correction

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -8,7 +8,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
-from dataetime import datetime
+from datetime import datetime
 
 
 @app.before_request
