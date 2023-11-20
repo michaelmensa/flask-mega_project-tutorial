f095cc391b715e6fbbaae6f7ddda122592dadd0f
michaelmensa <michaelmensa@live.com>
2023-11-20 21:10:32 +0000

    modified it to handle errors

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 
 app = Flask(__name__)
@@ -33,3 +33,19 @@ if not app.debug:
             )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+        ''' Receiving errors via email is nice, how about getting a log file
+        the application.'''
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',
+                                           maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(
+            loggin.Formatter('%(asctime)s %(levelname)s \
+            : %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
