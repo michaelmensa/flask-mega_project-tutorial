31210166bf95d5dee70ae00a7ddec51cdd5753b4
michaelmensa <michaelmensa@live.com>
2023-11-22 16:41:48 +0000

    pagination modifications - routes.py

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -35,7 +35,7 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('index', page=post.next_num) \
+    next_url = url_for('index', page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
@@ -51,7 +51,7 @@ def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
             page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('explore', page=post.next_num) \
+    next_url = url_for('explore', page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
