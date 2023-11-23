aeacb5e87b4dfa9cfe4e8ba9ab32b62a88669cfe
michaelmensa <michaelmensa@live.com>
2023-11-23 21:57:47 +0000

    added threading to the email.py module

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -4,13 +4,20 @@ Module - Email: Module for email support to clients
 from flask_mail import Message
 from app import app, mail
 from flask import render_template
+from threading import Thread
+
+
+def send_async_email(app, msg):
+    ''' send emails asynchronously wrapper function '''
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     ''' email sending wrapper function '''
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 def send_password_reset_email(user):
     ''' send password reset email function. args=user '''
